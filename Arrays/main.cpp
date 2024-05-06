4e234b5b2cd0ea9d6b7a0e546654dcd778031ee7
Алексей Семионов <alehaaaaa@mail.ru>
2024-05-06 17:05:17 +0300

    ShiftLeft DONE

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -2,19 +2,55 @@
 #include<iostream>
 using namespace std;
 
+#define tab "\t"
+
 void main()
 {
 	setlocale(LC_ALL, "");
 	const int SIZE = 5;
 	int arr[SIZE];
 
-	arr[2] = 2048;
-	cout << arr[2] << endl;
+	//arr[2] = 2048;
+	//cout << arr[2] << endl;
 
+	//ввод элементов массива с клавиатуры
+	cout << "Введите элементы массива: ";
 	for (int i = 0; i < SIZE; i++)
 	{
-		cout << arr[i] << "\t";
+		cin >> arr[i];
+	}
+
+	//вывод массива на экран
+	for (int i = 0; i < SIZE; i++)
+	{
+		cout << arr[i] << tab;
 	}
 	cout << endl;
 
+	//Вывод массива на экран в обратном порядке
+	for (int i = SIZE - 1; i >= 0; i--)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+
+	//Вычесление суммы элементов массива
+	int sum = 0;
+	for (int i = 0; i < SIZE; i++)
+	{
+		sum += arr[i];
+	}
+	cout << "Ссумма элементов массива: " << sum << endl;
+	cout << "Средние арифметическое: " << (double)sum / SIZE << endl;
+
+	// Поиск минимального значения
+	int min, max;
+	min = max = arr[0];
+	for (int i = 0; i < SIZE; i++)
+	{
+		if (arr[i] < min)min = arr[i];
+		if (arr[i] > max)max = arr[i];
+	}
+	cout << "Минимальное значение: " << min << endl;
+	cout << "Максимальное значение: " << max << endl;
 }
